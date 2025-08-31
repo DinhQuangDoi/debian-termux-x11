ff4e95bcaf9c53aa06784f911976c45fe95800c1
Đinh Quang Đới <dinhquangdoi99@gmail.com>
2025-09-01 01:58:11 +0700

    Create debian-x11.sh

---
+++ debian-x11.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt update
+apt install -y xfce4 xfce4-goodies dbus-x11 firefox-esr \
+               thunar thunar-archive-plugin p7zip-full \
+               pulseaudio-utils fonts-dejavu fonts-noto fonts-noto-cjk
+apt install -y mousepad vlc
